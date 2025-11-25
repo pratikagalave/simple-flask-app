c016df1b16025c16a797657e43945f3ab3605232
pratikagalave <pratikagalave123@gmail.com>
2025-11-25 14:22:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Auto-deploy test success! 🎉 Deployed directly from GitHub Actions to EC2."
+    return "Second auto-deploy test 🚀 — CI/CD pipeline is running perfectly!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
